60758551aee0f04fa80ff8c79f5e7adb660ec95f
Arya-kashyap <aryakashyap4188@gmail.com>
2025-08-21 13:08:48 +0530

    fixed scroll 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Signin from './components/Signin'
 
 function App() {
   return (
-    <div className="max-w-full max-h-dvh overflow-hidden">
+    <div className="max-w-full max-h-svh overflow-auto ">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<Signin />} />
