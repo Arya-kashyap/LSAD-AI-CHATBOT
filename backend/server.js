9e2a7ad61441bff09646595db9249b4aba2f2da4
Arya-kashyap <aryakashyap4188@gmail.com>
2025-08-29 21:08:59 +0530

    fixed server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { connectDB } from '../utils/dbConnect.js';
-import userRoute from '../routes/userRoute.js';
-import promptRoute from '../routes/promptRoute.js';
+import { connectDB } from './utils/dbConnect.js';
+import userRoute from './routes/userRoute.js';
+import promptRoute from './routes/promptRoute.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
